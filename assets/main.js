469340939a50f2aa3cc2d9548b43cb4a2cf7821a
Aditya Iyer <adityaiyer2007@gmail.com>
2024-08-11 16:32:23 +0530

    Submitting scores fixed

--- assets/main.js
+++ assets/main.js
@@ -61,3 +61,35 @@ winGame2 = () => {
     if (!games2.valueAsNumber) games2.value = 0;
     games2.value = games2.valueAsNumber + 1;
 }
+
+function submitData() {
+    data = {
+        matchid: window.location.pathname.split('/').pop(),
+        team1score: document.getElementById("team1score").valueAsNumber,
+        team2score: document.getElementById("team2score").valueAsNumber,
+        team1games: document.getElementById("team1games") ? document.getElementById("team1games").valueAsNumber : null,
+        team2games: document.getElementById("team2games") ? document.getElementById("team2games").valueAsNumber : null,
+        team1penscore: document.getElementById("team1penscore") ? document.getElementById("team1penscore").valueAsNumber : null,
+        team2penscore: document.getElementById("team2penscore") ? document.getElementById("team2penscore").valueAsNumber : null,
+        team1sets: [],
+        team2sets: []
+    };
+    for (let i = 1; i <= 6; i++) {
+        set1 = document.getElementById(`team1set${i}`);
+        set2 = document.getElementById(`team2set${i}`);
+        if (set1) data.team1sets.push(set1.value);
+        if (set2) data.team2sets.push(set2.value);
+    }
+    fetch('/portal/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(response => response.json())
+    .then(data => {
+        console.log('Success:', data);
+    }).catch((error) => {
+        console.error('Error:', error);
+    });
+}
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
+from threading import Lock
+
 import flask
 import json
 import os
 
 app = flask.Flask(__name__)
 
+rwlock = Lock()
+
 @app.route('/portal/<matchid>')
-def index(matchid):
+def portal(matchid):
     with open("matchindex.json") as fl:
         json_data = json.load(fl)
         if matchid not in json_data.keys() or\
@@ -20,6 +24,25 @@ def index(matchid):
     data = json_data["data"]
     config = json_data["config"]
     return flask.render_template('index.html', sport=sport, school1=school1, school2=school2, data=data, config=config)
+
+@app.route('/portal/submit/', methods=["POST"])
+def portal_submit():
+    data = flask.request.get_json(force=True)
+    if "matchid" not in data.keys(): return flask.Response(status=400)
+    matchid = data.pop("matchid")
+    with open("matchindex.json") as fl:
+        json_data = json.load(fl)
+        if matchid not in json_data.keys() or\
+           not os.path.exists(json_data[matchid]):
+            return flask.Response(status=400)
+        fp = json_data[matchid]
+    with open(fp) as fl:
+        json_data = json.load(fl)
+    json_data["data"] = data
+    with rwlock:
+        with open(fp, mode='w') as fl:
+            json.dump(json_data, fl, indent=" ")
+    return {"success": True}
 
 @app.route('/portal/main.js')
 def mainjs(): return flask.send_file("./assets/main.js")
